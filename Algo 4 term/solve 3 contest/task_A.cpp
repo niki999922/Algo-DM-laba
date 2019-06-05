bf3bb1c0a125d9575a0c095ab2a68a22c34f76b1
Nikita Kochetkov <niki999922@yandex.ru>
2019-06-05 08:56:46 +0300

    Update task_A.cpp

--- Algo 4 term/solve 3 contest/task_A.cpp
+++ Algo 4 term/solve 3 contest/task_A.cpp
@@ -7,6 +7,7 @@
 #include <algorithm>
 #include <map>
 #include <set>
+#include <sstream>
 #include <cmath>
 
 using namespace std;
@@ -15,31 +16,37 @@ using namespace std;
 typedef long long ll;
 typedef unsigned long long ull;
 typedef unsigned long ul;
+typedef unsigned int ui;
 typedef unsigned char byte;
 typedef vector<int> vi;
 typedef vector<ll> vll;
 
 const int INF = INT32_MAX - 1;
 const ll MOD = 998244353;
+const ll N = 20000001;
 
-int n, num;
+int n, a;
+vector<bool> prime(N, false);
 
-bool isPrime(int num) {
-    for (size_t j = 2; j <= sqrt(num); ++j) {
-        if (num % j == 0) {
-            return false;
+void pre_count() {
+    for (size_t i = 2; i * i < N; ++i) {
+        if (!prime[i]) {
+            for (size_t j = i * i; j < N; j += i) {
+                prime[j] = 1;
+            }
         }
     }
-    return true;
 }
+
 
 int main() {
     std::ios_base::sync_with_stdio(false);
     cin.tie(nullptr);
+    pre_count();
     cin >> n;
     for (size_t i = 0; i < n; ++i) {
-        cin >> num;
-        if (isPrime(num)) {
+        cin >> a;
+        if (!prime[a]) {
             cout << "YES\n";
         } else {
             cout << "NO\n";
