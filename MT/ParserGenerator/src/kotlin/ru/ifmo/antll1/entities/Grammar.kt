7ec3b085e7da22b95063858c5b3daf3f9f0a2fe3
nikita <nikita.kochetkov@jetbrains.com>
2020-01-09 02:50:14 +0300

    set main for first parsing

--- MT/ParserGenerator/src/kotlin/ru/ifmo/antll1/entities/Grammar.kt
+++ MT/ParserGenerator/src/kotlin/ru/ifmo/antll1/entities/Grammar.kt
@@ -8,4 +8,74 @@ class Grammar {
     lateinit var headers : CodeStep
     lateinit var startRule: String
     lateinit var grammarName: String
+
+    fun buildFirst() {
+
+    }
+
+}
+
+
+
+fun main() {
+    val rules = mutableListOf<Rule>()
+    val e = Rule("E")
+    var c =Condition()
+    c.steps.add(RuleTermStep("r","T"))
+    c.steps.add(CodeStep("code code 1"))
+    c.steps.add(RuleTermStep("","E1"))
+    c.steps.add(CodeStep("code code 2"))
+    e.conditions.add(c)
+    rules.add(e)
+
+    val e1 = Rule("E1")
+    c =Condition()
+    c.steps.add(RuleTermStep("","PLUS"))
+    c.steps.add(RuleTermStep("","T"))
+    c.steps.add(CodeStep("code code 3"))
+    c.steps.add(RuleTermStep("","E1"))
+    e1.conditions.add(c)
+
+    c =Condition()
+    c.steps.add(CodeStep(""))
+    e1.conditions.add(c)
+    rules.add(e1)
+
+    val t = Rule("T")
+    c =Condition()
+    c.steps.add(RuleTermStep("","F"))
+    c.steps.add(RuleTermStep("p","T1"))
+    c.steps.add(CodeStep("code code 4"))
+    t.conditions.add(c)
+    rules.add(t)
+
+
+
+    val t1 = Rule("T1")
+    c =Condition()
+    c.steps.add(RuleTermStep("","MUL"))
+    c.steps.add(RuleTermStep("","F"))
+    c.steps.add(RuleTermStep("","T1"))
+    t1.conditions.add(c)
+
+    c =Condition()
+    c.steps.add(CodeStep(""))
+    t1.conditions.add(c)
+    rules.add(t1)
+
+
+    val f = Rule("F")
+    c =Condition()
+    c.steps.add(RuleTermStep("","OP_B"))
+    c.steps.add(RuleTermStep("","E"))
+    c.steps.add(RuleTermStep("","CL_B"))
+    f.conditions.add(c)
+
+    c = Condition()
+    c.steps.add(CodeStep("NUM"))
+    f.conditions.add(c)
+    rules.add(f)
+
+
+    rules.add(Rule("E"))
 }
--- MT/ParserGenerator/src/kotlin/ru/ifmo/antll1/entities/Step.kt
+++ MT/ParserGenerator/src/kotlin/ru/ifmo/antll1/entities/Step.kt
@@ -1,6 +1,4 @@
 package ru.ifmo.antll1.entities
 
-import ru.ifmo.antll1.entities.StepType
-
 open class Step(val stepType: StepType) {
 }
